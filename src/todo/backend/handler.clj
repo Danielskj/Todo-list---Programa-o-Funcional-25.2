9a1559f98d1fa3cf8db5e865158292f49dbe7322
Carlos Daniel <carlosdiel16@gmail.com>
2025-11-24 22:21:44 -0300

    alteração 4  backend - handler.clj

--- src/todo/backend/handler.clj
+++ src/todo/backend/handler.clj
@@ -1,21 +1,43 @@
 (ns todo.backend.handler
+  "Handlers das rotas da API"
   (:require
     [todo.backend.db :as db]
-    [reitit.ring :as ring]
-    [ring.middleware.json :refer [wrap-json-body wrap-json-response]]))
+    [clojure.string :as str]))
 
-(def app
-  (-> (ring/ring-handler
-        (ring/router
-          [["/api"
-            ["/todos"
-             {:get  (fn [_]
-                      {:status 200
-                       :body (db/get-all-todos)})
+;; ------------------------------
+;; HELLO HANDLER
+;; ------------------------------
+(defn hello-handler
+  [_request]
+  {:status 200
+   :body "Hello, World!"})
 
-              :post (fn [req]
-                      (let [todo (:body req)]
-                        {:status 201
-                         :body (db/create-todo todo)}))}]]]))
-      wrap-json-response
-      (wrap-json-body {:keywords? true})))
+;; ------------------------------
+;; LISTAR TODOS
+;; ------------------------------
+(defn list-todos-handler
+  "Handler para GET /api/todos."
+  [_request]
+  {:status 200
+   :body {:todos (db/get-all-todos)}})
+
+;; ------------------------------
+;; CRIAR TODO
+;; ------------------------------
+(defn create-todo-handler
+  "Handler para POST /api/todos."
+  [{:keys [body]}]
+  (let [title (:title body)]
+    (if (and title (not (str/blank? title)))
+      (let [new-todo (db/create-todo body)]
+        {:status 201
+         :body new-todo})
+      {:status 400
+       :body {:error "O campo 'title' é obrigatório."}})))
+(defn toggle-todo-handler
+  "Handler para 'alternar' o status de um todo."
+  [request]
+  (let [id (-> request :path-params :id Integer/parseInt)]
+    (if-let [updated-todo (db/toggle-todo! id)]
+      {:status 200 :body updated-todo}
+      {:status 404 :body {:error "Todo não encontrado"}})))
