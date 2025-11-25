514b12710b863b087449f8142cb2c116ff2c6655
Carlos Daniel <carlosdiel16@gmail.com>
2025-11-24 22:17:21 -0300

    alteração 3 backend - db_config e db.clj

--- src/todo/backend/db.clj
+++ src/todo/backend/db.clj
@@ -1,19 +1,14 @@
 (ns todo.backend.db
   "Este namespace gerencia os dados dos 'todos'
-   usando um banco de dados persistente SQLite.")
+   usando um banco de dados persistente SQLite."
+  (:require [next.jdbc :as jdbc]
+            [clojure.string :as str]))
 
-(require '[next.jdbc :as jdbc]
-         '[clojure.string :as str])
-
-;; --- 1. A Configuração (Especificação do Banco) ---
-;; Definimos como nos conectar ao nosso banco de dados.
-;; Ele será salvo em um arquivo chamado "prod.db".
+;; --- 1. A Configuração do Banco ---
 (def db-spec {:dbtype "sqlite"
               :dbname "prod.db"})
 
-;; --- 2. Função de Inicialização ---
-;; Esta função cria nossa tabela "todos" se ela ainda não existir.
-;; Vamos chamá-la em nosso 'core.clj' quando o servidor iniciar.
+;; --- 2. Inicialização ---
 (defn initialize-database!
   "Cria a tabela 'todos' no banco de dados se ela não existir."
   []
@@ -27,29 +22,33 @@
     )
   "]))
 
-;; --- 3. As Novas Funções (Substituindo os 'atoms') ---
+;; --- 3. Funções do Banco ---
 
-(defn get-all-todos
-  "Retorna uma lista com todos os 'todos' no banco."
-  []
-  ;; O 'execute!' roda o SQL e já retorna os mapas Clojure!
+(defn get-all-todos []
   (jdbc/execute! db-spec ["SELECT * FROM todos ORDER BY created_at DESC"]))
 
-(defn create-todo
-  "Cria um novo 'todo', salva no banco e o retorna."
-  [todo-data] ;; ex: {:title "Minha tarefa", :description "..."}
-  (let [;; Convertemos o booleano para 0 (false)
-        todo-map (assoc todo-data
+(defn create-todo [todo-data]
+  (let [todo-map (assoc todo-data
                         :completed 0
                         :created-at (str (java.time.Instant/now)))
-        ;; 'execute-one!' insere e já retorna o item criado
-        result (jdbc/execute-one! db-spec ["
-          INSERT INTO todos (title, description, completed, created_at)
-          VALUES (?, ?, ?, ?)"
-          (:title todo-map)
-          (:description todo-map)
-          (:completed todo-map)
-          (:created-at todo-map)
-          ;; :returning "*" faz o SQLite retornar o item inserido
-          ] {:returning "*"})]
+        result (jdbc/execute-one!
+                 db-spec
+                 ["INSERT INTO todos (title, description, completed, created_at)
+                   VALUES (?, ?, ?, ?)"
+                  (:title todo-map)
+                  (:description todo-map)
+                  (:completed todo-map)
+                  (:created-at todo-map)]
+                 {:returning "*"})]
     result))
+(defn toggle-todo!
+  "Alterna o status 'completed' de um todo no banco."
+  [id]
+  ;; (1 - completed) é um truque SQL para inverter 0 -> 1 e 1 -> 0.
+  (jdbc/execute-one! db-spec ["
+    UPDATE todos
+    SET completed = (1 - completed)
+    WHERE id = ?
+    RETURNING *"
+    id
+  ]))
