53a657b7471bfc116c08648c5d3167e2383cb954
Carlos Daniel <carlosdiel16@gmail.com>
2025-11-24 22:09:12 -0300

    alteração 2 backend - core.clj

--- src/todo/backend/core.clj
+++ src/todo/backend/core.clj
@@ -1,38 +1,58 @@
 (ns todo.backend.core
-  (:require [ring.adapter.jetty :as jetty]
-            [reitit.ring :as ring]
-            [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
-            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
-            [ring.middleware.params :refer [wrap-params]]
-            [ring.middleware.cors :refer [wrap-cors]]
-            [todo.backend.handler :as handler]
-            ;; --- ADICIONE ESTA LINHA ---
-            [todo.backend.db :as db]))
-  (:gen-class))
-
-(ns todo.backend.core
   (:require
-    [todo.backend.handler :refer [app]]
-    [ring.adapter.jetty :refer [run-jetty]])
+    [ring.middleware.params :refer [wrap-params]]
+    [ring.adapter.jetty :as jetty]
+    [reitit.ring :as ring]
+    [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
+    [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+    [ring.middleware.cors :refer [wrap-cors]]
+    [todo.backend.handler :as handler]
+    [todo.backend.db :as db])
   (:gen-class))
 
-(defonce server (atom nil))
 
-(defn start-server []
-  (reset! server
-          (run-jetty app {:port 3000 :join? false}))
-  (println "Servidor iniciado em http://localhost:3000"))
+(def app-routes
+  (ring/router
+    ["/api"
+     ["/hello" {:get {:handler handler/hello-handler}}]
 
-(defn stop-server []
-  (when @server
-    (.stop @server)
-    (reset! server nil)
-    (println "Servidor parado.")))
+     ["/todos"
+      {:get {:handler handler/list-todos-handler}
+       :post {:handler handler/create-todo-handler}}]
 
-;; CÓDIGO CORRIGIDO
+     ["/todos/:id/toggle"
+      {:post {:handler handler/toggle-todo-handler}}]]))
+
+
+(def app
+  (ring/ring-handler
+    app-routes
+    (ring/create-default-handler)
+    {:middleware [
+                  ;; ---- CORS (primeiro sempre) ----
+                  [wrap-cors
+                   :access-control-allow-origin [#"http://localhost:8000"]
+                   :access-control-allow-methods [:get :post :put :delete]
+                   :access-control-allow-headers ["Content-Type"]]
+
+                  ;; ---- Outros middlewares ----
+                  wrap-json-response
+                  [wrap-json-body {:keywords? true}]
+                  wrap-keyword-params
+                  wrap-params
+                  ]}))
+
+
+;; Servidor
+
+(defn start-server [port]
+  (println (str "Servidor iniciado na porta " port))
+  (jetty/run-jetty #'app {:port port :join? false}))
+
 (defn -main [& args]
   (let [port (Integer/parseInt (or (first args) "3000"))]
-    ;; --- ADICIONE ESTA LINHA ---
     (db/initialize-database!) ;; Garante que a tabela exista
-    ;; --------------------------
     (start-server port)))
+
+
+
